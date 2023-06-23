89c49c2d30af76027ce1dd225036e8dd32dacb95
Olga Nastaja <olga.vlasova321@gmail.com>
2023-06-23 14:28:26 +0100

    add TrackList, Track

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import { SearchBar } from '../SearchBar';
-import { SearchResults } from '../SearchResults';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </div >
+    );
+  }
 }
 
 export default App;
